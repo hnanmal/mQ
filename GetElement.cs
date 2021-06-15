d04a96d511867f29a053ee9a2d0d89bf0a779011
pjmk007 <pjmk007@gmail.com>
2021-06-15 17:27:05 +0900

    초기 커밋.

---
+++ GetElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Geometry;
+using Autodesk.Revit;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+
+namespace mQ
+{
+    public class GetElement
+    {
+        //List<ElementId> ids = new FilteredElementCollector(document).OfCategory(BuiltInCategory.OST_Walls).ToElementIds().ToList();
+    }
+}
---
+++ ZeroTouchTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Runtime;
+using Autodesk.DesignScript.Interfaces;
+using Autodesk.DesignScript.Geometry;
+
+namespace mQ
+{
+    public class ZeroTouchTest
+    {
+        private double _x;
+        private double _y;
+        private double _z;
+
+        internal ZeroTouchTest(double x, double y, double z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+        public static Line ByTwoDoubles(double x = 0, double y = 0, double z = 0)
+        {
+            Point p1 = Point.ByCoordinates(x, y, z);
+            Point p2 = Point.ByCoordinates(x + 10, y + 10, z + 10);
+            Line line = Line.ByStartPointEndPoint(p1, p2);
+            p1.Dispose();
+            p2.Dispose();
+            return line;
+        }
+        public static Point GetMidPoint(Line InputLine)
+        {
+            Point MidPoint;
+            MidPoint = InputLine.PointAtParameter(0.5);
+            return MidPoint;
+        }
+    }
+}
