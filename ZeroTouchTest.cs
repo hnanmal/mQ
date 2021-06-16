004f6b45348863dc96ffc4d34e1c51defc9e1636
pjmk007 <60164230+pjmk007@users.noreply.github.com>
2021-06-16 23:58:49 +0900

    getCategory 기능 추가

--- ZeroTouchTest.cs
+++ ZeroTouchTest.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Autodesk.DesignScript.Runtime;
 using Autodesk.DesignScript.Interfaces;
 using Autodesk.DesignScript.Geometry;
+using Autodesk.Revit.DB;
+using Revit.Elements;
 
 namespace mQ
 {
@@ -21,20 +23,62 @@ namespace mQ
             _y = y;
             _z = z;
         }
-        public static Line ByTwoDoubles(double x = 0, double y = 0, double z = 0)
+        public static Autodesk.DesignScript.Geometry.Line ByTwoDoubles(double x = 0, double y = 0, double z = 0)
         {
-            Point p1 = Point.ByCoordinates(x, y, z);
-            Point p2 = Point.ByCoordinates(x + 10, y + 10, z + 10);
-            Line line = Line.ByStartPointEndPoint(p1, p2);
+            Autodesk.DesignScript.Geometry.Point p1 = Autodesk.DesignScript.Geometry.Point.ByCoordinates(x, y, z);
+            Autodesk.DesignScript.Geometry.Point p2 = Autodesk.DesignScript.Geometry.Point.ByCoordinates(x + 10, y + 10, z + 10);
+            Autodesk.DesignScript.Geometry.Line line = Autodesk.DesignScript.Geometry.Line.ByStartPointEndPoint(p1, p2);
             p1.Dispose();
             p2.Dispose();
             return line;
         }
-        public static Point GetMidPoint(Line InputLine)
+        public static Autodesk.DesignScript.Geometry.Point MakeQpoint(Double x = 1, Double y = 1, Double z = 1)
         {
-            Point MidPoint;
+            Autodesk.DesignScript.Geometry.Point QPoint = Autodesk.DesignScript.Geometry.Point.ByCoordinates(x, y, z);
+            return QPoint;
+        }
+        public static Autodesk.DesignScript.Geometry.Line MakeQline(Autodesk.DesignScript.Geometry.Point StartPoint, Autodesk.DesignScript.Geometry.Point EndPoint)
+        {
+            Autodesk.DesignScript.Geometry.Line QLine = Autodesk.DesignScript.Geometry.Line.ByStartPointEndPoint(StartPoint, EndPoint);
+            return QLine;
+        }
+        public static Autodesk.DesignScript.Geometry.Point GetMidPoint(Autodesk.DesignScript.Geometry.Line InputLine)
+        {
+            Autodesk.DesignScript.Geometry.Point MidPoint;
             MidPoint = InputLine.PointAtParameter(0.5);
             return MidPoint;
+        }
+
+        [MultiReturn(new[] {"Line","MiddlePoint"})]
+        public static Dictionary<string, object> MakeLine_MidPoint(Autodesk.DesignScript.Geometry.Point StartPoint, Autodesk.DesignScript.Geometry.Point EndPoint)
+        {
+            Autodesk.DesignScript.Geometry.Line line;
+            line = Autodesk.DesignScript.Geometry.Line.ByStartPointEndPoint(StartPoint, EndPoint);
+
+            Autodesk.DesignScript.Geometry.Point MidPoint;
+            MidPoint = line.PointAtParameter(0.5);
+
+            Dictionary<string, object> MultiOutPutPort;
+            MultiOutPutPort = new Dictionary<string, object>
+            {
+                {"Line", line },
+                {"MiddlePoint", MidPoint }
+            };
+            return MultiOutPutPort;
+        }
+    }
+
+    public class Revit_Elements
+    {
+        private Revit_Elements()
+        {
+
+        }
+        public static Revit.Elements.Category GetCategory(Revit.Elements.Element InputElement)
+        {
+            Revit.Elements.Category CatFromInput;
+            CatFromInput = InputElement.GetCategory;
+            return CatFromInput;
         }
     }
 }
