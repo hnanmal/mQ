947480d2a60355b647252f7da8c68b8d742fe5fd
pjmk007 <60164230+pjmk007@users.noreply.github.com>
2021-06-22 00:03:26 +0900

    Update GetElement.cs

--- GetElement.cs
+++ GetElement.cs
@@ -31,7 +31,6 @@ namespace mQ
             // Unwrapping part for InputElement
             Autodesk.Revit.DB.Element UnwrappedElements;
             UnwrappedElements = InputElements.InternalElement;
-            //
 
             Autodesk.Revit.DB.ElementId UnwrappedElementId;
             UnwrappedElementId = UnwrappedElements.Id;
@@ -62,11 +61,13 @@ namespace mQ
         {
             Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
             //setup collector and filter
-            var WallCollector = new FilteredElementCollector(doc);
+            var ElementCollector = new FilteredElementCollector(doc);
             var CategoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_Walls);
-            //collect all mechanical equipment
-            var CountedWallsNumber = WallCollector.WherePasses(CategoryFilter).GetElementCount();
 
+            var CountedWallsNumber = ElementCollector
+                .WherePasses(CategoryFilter)
+                .WhereElementIsNotElementType()
+                .GetElementCount();
 
             return CountedWallsNumber;
         }
@@ -75,10 +76,14 @@ namespace mQ
             Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
             //setup collector and filter
             var ElementCollector = new FilteredElementCollector(doc);
+            var CategoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_Walls);
+
+            var Walls = ElementCollector
+                .WherePasses(CategoryFilter)
+                .WhereElementIsNotElementType()
+                .Cast<Autodesk.Revit.DB.Element>();
 
             List<Revit.Elements.Element> OutList = new List<Revit.Elements.Element>();
-
-            var Walls = ElementCollector.OfClass(typeof(Autodesk.Revit.DB.Wall)).ToElements();
 
             foreach (var i in Walls)
             {
@@ -94,9 +99,12 @@ namespace mQ
             var ElementCollector = new FilteredElementCollector(doc);
             var CategoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_StructuralColumns);
 
-            List<Revit.Elements.Element> OutList = new List<Revit.Elements.Element>();
+            var StrColumns = ElementCollector
+                .WherePasses(CategoryFilter)
+                .WhereElementIsNotElementType()
+                .Cast<Autodesk.Revit.DB.Element>();
 
-            var StrColumns = ElementCollector.WherePasses(CategoryFilter).WhereElementIsNotElementType().Cast<Autodesk.Revit.DB.Element>();
+            List<Revit.Elements.Element> OutList = new List<Revit.Elements.Element>();
 
             foreach (var i in StrColumns)
             {
@@ -113,9 +121,12 @@ namespace mQ
             var ElementCollector = new FilteredElementCollector(doc);
             var CategoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_StructuralFoundation);
 
-            List<Revit.Elements.Element> OutList = new List<Revit.Elements.Element>();
+            var StrFoundations = ElementCollector
+                .WherePasses(CategoryFilter)
+                .WhereElementIsNotElementType()
+                .Cast<Autodesk.Revit.DB.Element>();
 
-            var StrFoundations = ElementCollector.WherePasses(CategoryFilter).WhereElementIsNotElementType().Cast<Autodesk.Revit.DB.Element>();
+            List<Revit.Elements.Element> OutList = new List<Revit.Elements.Element>();
 
             foreach (var i in StrFoundations)
             {
@@ -131,13 +142,16 @@ namespace mQ
             var ElementCollector = new FilteredElementCollector(doc);
             var CategoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_StructuralFoundation);
 
-            List<Revit.Elements.Element> OutList = new List<Revit.Elements.Element>();
+            var StrFoundations = ElementCollector
+                .WherePasses(CategoryFilter)
+                .WhereElementIsNotElementType()
+                .Cast<Autodesk.Revit.DB.Element>();
 
-            var StrFoundations = ElementCollector.WherePasses(CategoryFilter).WhereElementIsNotElementType().Cast<Autodesk.Revit.DB.Element>();
             var FoundationFloors = from i in StrFoundations
                                    where i.Name.ToString().Contains("SOG")
                                    select i;
 
+            List<Revit.Elements.Element> OutList = new List<Revit.Elements.Element>();
 
             foreach (var i in FoundationFloors)
             {
@@ -146,6 +160,11 @@ namespace mQ
 
             return OutList;
         }
+        /// <summary>
+        /// Structural Foundation Element중 해당 문자열이 패밀리 이름에 포함된 모든 객체를 찾는 노드
+        /// </summary>
+        /// <param name="familyName">찾고 싶은 문자열 기입</param>
+        /// <returns>필터링된 revit element 반환</returns>
         public static List<Revit.Elements.Element> GetAllStrFoundationsByName(string familyName)
         {
             Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
@@ -153,15 +172,46 @@ namespace mQ
             var ElementCollector = new FilteredElementCollector(doc);
             var CategoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_StructuralFoundation);
 
+            var StrFoundations = ElementCollector
+                .WherePasses(CategoryFilter)
+                .WhereElementIsNotElementType()
+                .Cast<Autodesk.Revit.DB.Element>();
+
+            var FoundationFloors = from e in StrFoundations
+                                   where e.Name.ToString().Contains(familyName)
+                                   select e;
+
             List<Revit.Elements.Element> OutList = new List<Revit.Elements.Element>();
 
-            var StrFoundations = ElementCollector.WherePasses(CategoryFilter).WhereElementIsNotElementType().Cast<Autodesk.Revit.DB.Element>();
-            var FoundationFloors = from i in StrFoundations
-                                   where i.Name.ToString().Contains(familyName)
-                                   select i;
-
-
             foreach (var i in FoundationFloors)
+            {
+                OutList.Add(i.ToDSType(true));
+            }
+
+            return OutList;
+        }
+        public static List<Revit.Elements.Element> GetGroupedAllWallsElementByType()
+        {
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+            //setup collector and filter
+            var ElementCollector = new FilteredElementCollector(doc);
+            var CategoryFilter = new ElementCategoryFilter(BuiltInCategory.OST_Walls);
+
+            var walls = ElementCollector
+                .WherePasses(CategoryFilter)
+                .WhereElementIsNotElementType()
+                .Cast<Autodesk.Revit.DB.Element>()
+                .GroupBy(x => x.GetValidTypes());
+
+            var qWalls = from e in walls
+                         select e;
+            var qqWalls = qWalls
+                .SelectMany(group => group)
+                .ToList();
+
+            List<Revit.Elements.Element> OutList = new List<Revit.Elements.Element>();
+
+            foreach (var i in qqWalls)
             {
                 OutList.Add(i.ToDSType(true));
             }
