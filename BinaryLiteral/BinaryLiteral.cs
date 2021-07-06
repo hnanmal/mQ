e9a2c820d71f32f0ab88f7b2773e3ee5f4332ada
pjmk007 <60164230+pjmk007@users.noreply.github.com>
2021-07-06 23:20:59 +0900

    as

---
+++ BinaryLiteral/BinaryLiteral.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class BinaryLiteral
+{
+    static void Main()
+    {
+        byte b1 = 0b0010; // 이진수 0010 -> 십진수 2
+        byte b2 = 0b1100; // 이진수 1100 -> 십진수 8
+
+        Console.WriteLine($"10진수: {b1}"); // 십진수 2
+        Console.WriteLine($"10진수: {b2}"); // 십진수 12
+        
+        Console.WriteLine($"16진수: {b1:X}"); // 16진수 2
+        Console.WriteLine($"16진수: {b2:X}"); // 16진수 C
+
+    }
+}
+
---
+++ TupleLiteral/TupleLiteral.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class TupleLiteral
+{
+    static void Main()
+    {
+        var fhd = (1920, 1080);
+        System.Console.WriteLine($"Full HD: {fhd.Item1} * {fhd.Item2}");
+
+        var uhd = (Width: 3840, Height: 2160);
+        System.Console.WriteLine($"4K UHD: {uhd.Width} * {uhd.Height}");
+
+        (ushort Width, ushort Height) hd = (1366, 768);
+        System.Console.WriteLine($"HD: {hd.Width} * {hd.Height}");
+        System.Console.WriteLine($"형식: {hd.Width.GetType()} * {hd.Height.GetType()}");
+    }
+}
