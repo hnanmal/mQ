efbec0585c9760495d06788b7a3d58f4a8faaf56
pjmk007 <60164230+pjmk007@users.noreply.github.com>
2021-11-17 17:39:48 +0900

    hdgsdf

---
+++ Laboratory/Baekjun/Baekjun/q2739.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjun
+{
+    class q2739
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
