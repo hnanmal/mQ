1115d7df8734348539e728466e2c0851363dcaa0
pjmk007 <pjmk007@gmail.com>
2021-12-19 20:30:41 +0900

    Create SelectAllElementsExcept.py

---
+++ Hdynlib/Revit/Elements/Filtering/SelectAllElementsExcept.py
@@ -0,0 +1,20 @@
+# Load the Python Standard and DesignScript Libraries
+import sys
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# The inputs to this node will be stored as a list in the IN variables.
+dataEnteringNode = IN
+
+bln_exceptTarget = IN[0]
+input_elems = IN[1]
+targetString = IN[2]
+
+# Place your code below this line
+if bln_exceptTarget:
+    result = list(map(lambda x: x, input_elems))
+else:
+    result = list(filter(lambda x: targetString not in x.Name, input_elems))
+# Assign your output to the OUT variable.
+OUT = result
