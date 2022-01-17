40af74513f79910c5a8a62b35b16c68f7454124b
pjmk007 <pjmk007@gmail.com>
2022-01-17 17:31:02 +0900

    asdfadf

--- Hdynlib/General/StringFromWeb.py
+++ Hdynlib/General/StringFromWeb.py
@@ -8,6 +8,7 @@ import urllib.request
 # The inputs to this node will be stored as a list in the IN variables.
 dataEnteringNode = IN
 inputURL = ""
+ref = IN[0]
 
 # Place your code below this line
 req = urllib.request
@@ -16,4 +17,4 @@ data = req.urlopen(inputURL)
 result = data.read().decode("utf-8")
 
 # Assign your output to the OUT variable.
-OUT = result
+OUT = [result,ref]
