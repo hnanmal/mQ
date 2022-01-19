2c6fa79b362eca15e9998d442a94c418fae4f2c5
pjmk007 <pjmk007@gmail.com>
2022-01-19 16:48:04 +0900

    Update 되메우기산출함수.py

--- Hdynlib/Calculation/CalculateAlgorithm/되메우기산출함수.py
+++ Hdynlib/Calculation/CalculateAlgorithm/되메우기산출함수.py
@@ -56,13 +56,13 @@ def 되메우기산출함수(input):
         exca_solid = refFunc(input)[0]
         fdn_solid = Solid.ByUnion(list(chain(*[i.Geometry() for i in allIsoFdns])))
         peds_solid = Solid.ByUnion(allPedsGeo)
-        TGs_solid = Solid.ByUnion(allTGsGeo)
+        # TGs_solid = Solid.ByUnion(allTGsGeo)
         SOGs_solid = Solid.ByUnion(allSOGsGeo)
         #target = exca_solid.Difference(fdn_solid)
         _target1 = exca_solid.Difference(fdn_solid)
         _target2 = _target1.Difference(peds_solid)
-        _target3 = _target2.Difference(TGs_solid)
-        target = _target3.Difference(SOGs_solid)
+        # _target3 = _target2.Difference(TGs_solid)
+        target = _target2.Difference(SOGs_solid)
         
 
     else:
