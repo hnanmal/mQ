afac5b82996227624afb1c2989f51a41aecbb5ef
pjmk007 <pjmk007@gmail.com>
2022-01-25 17:36:57 +0900

    adffdfdf

---
+++ Hdynlib/General/ExportExcel.py
@@ -0,0 +1,166 @@
+# Load the Python Standard and DesignScript Libraries
+import sys
+import os
+#sys.path.append(r'C:\Users\HEC\AppData\Local\Programs\Python\Python39\Lib\site-packages')
+#sys.path.append(r'C:\Users\pjmk0\AppData\Local\Programs\Python\Python39\Lib\site-packages')
+sys.path.append(os.getenv('LOCALAPPDATA').replace('\\','\\\\') + r'\Programs\Python\Python39\Lib\site-packages')
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+from collections import Iterable
+
+def iterable(obj):
+    return isinstance(obj, Iterable)
+
+
+# The inputs to this node will be stored as a list in the IN variables.
+dataEnteringNode = IN
+
+exportBln = IN[0]
+pjtinfo = IN[1][0]
+PjtName = pjtinfo[0]
+BldgName = pjtinfo[1]
+SheetName = IN[1][1]
+ListIn = IN[2]
+
+# Place your code below this line
+import datetime
+from datetime import date, timedelta
+Today = date.today()
+import openpyxl
+from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment, Color, numbers
+
+sample_items = ListIn
+
+
+def write_to_file(filepath):
+    wb = openpyxl.Workbook()
+    for sheet in wb.sheetnames:
+        wb.remove(wb[sheet])
+
+    thin = Side(border_style="thin", color="000000")
+    double = Side(border_style="double", color="000000")
+
+    ws = wb.create_sheet(title= f'{SheetName}', index=0)
+
+    ws.merge_cells('A1:L1')
+    ws['A1'] = f'{SheetName} Category Calculation Sheet ({PjtName}_{BldgName})'
+    cell = ws['A1']
+    cell.font = openpyxl.styles.Font(color='000000', size=20)
+    cell.fill = openpyxl.styles.fills.PatternFill(patternType='solid',
+                                                  fgColor=openpyxl.styles.colors.Color(rgb='fff3b0'))
+
+    ws['A2'] = 'Item Number'
+    ws['A2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    #ws['B2'] = 'Member Name'
+    #ws['B2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    #ws['C2'] = 'Category'
+    #ws['C2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    ws['B2'] = 'Family Type'
+    ws['B2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    ws['C2'] = 'WorkMaster Code'
+    ws['C2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    ws['D2'] = 'Description'
+    ws['D2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    #ws['F2'] = 'WorkMaster Description'
+    #ws['F2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    #ws['G2'] = 'Unit'
+    #ws['G2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    #ws['H2'] = '계수'
+    #ws['H2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    #ws['I2'] = '계산 참조값'
+    #ws['I2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    ws['E2'] = 'Quantities'
+    ws['E2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    ws['F2'] = 'Unit'
+    ws['F2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    ws['G2'] = 'Unit Price'
+    ws['G2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+    ws['H2'] = 'Total Price'
+    ws['H2'].font = openpyxl.styles.Font(b=True, color='000000', size=12)
+
+
+## 칼럼별 너비 조정 구간
+#    for col in range(ws.max_column):
+#        ws.column_dimensions[chr(ord('A') + col)].width = 15
+    ws.column_dimensions['A'].width = 15
+    ws.column_dimensions['B'].width = 45
+    ws.column_dimensions['C'].width = 25
+    ws.column_dimensions['D'].width = 60
+    ws.column_dimensions['E'].width = 15
+    ws.column_dimensions['F'].width = 20
+    ws.column_dimensions['G'].width = 20
+    ws.column_dimensions['H'].width = 20
+    #ws.column_dimensions['G'].width = 9
+    #ws.column_dimensions['H'].width = 9
+    #ws.column_dimensions['H'].hidden = True
+    #ws.column_dimensions['I'].width = 12
+    #ws.column_dimensions['I'].hidden = True
+    #ws.column_dimensions['J'].width = 12
+    #ws.column_dimensions['K'].width = 12
+    #ws.column_dimensions['L'].width = 12
+
+###
+    start_row = 3
+    for (row, item) in enumerate(sample_items, start_row):
+        cell = ws.cell(row=row, column=1, value=row - 2)
+        #cell.number_format = openpyxl.styles.numbers.builtin_format_code(0)
+        cell.alignment = openpyxl.styles.Alignment(horizontal='center', vertical='center')
+
+#        cell = ws.cell(row=row, column=2, value=item[0])
+#        cell.number_format = openpyxl.styles.numbers.FORMAT_DATE_YYYYMMDD2  # 'yyyy-mm-dd'
+        ws.cell(row=row, column=2, value=item[0]) ## Family Type 칼럼
+
+        ws.cell(row=row, column=3, value=item[1]) ## WorkMaster Code 칼럼
+        
+        ws.cell(row=row, column=4, value=item[2]) ## WorkMaster Code 칼럼
+        
+        ws.cell(row=row, column=5, value=item[3]) ## Quantities 칼럼
+        cell.number_format = '#,##0.00'
+        
+        ws.cell(row=row, column=6, value=item[4]) ## Unit 칼럼
+        
+        #cell = ws.cell(row=row, column=8, value=item[6]) ## 계수 칼럼
+        #cell.number_format = openpyxl.styles.numbers.builtin_format_code(3)  # '#,##0'
+        
+        #cell = ws.cell(row=row, column=9, value=item[7]) ## 계산 참조값 칼럼
+        #cell.number_format = openpyxl.styles.numbers.builtin_format_code(3)  # '#,##0'
+        
+        #cell = ws.cell(row=row, column=10, value=item[8]) ## Quantities 칼럼
+        #cell.number_format = openpyxl.styles.numbers.builtin_format_code(3)  # '#,##0'
+        #cell.font = openpyxl.styles.Font(b=True, color='000000', size=11)
+
+        cell = ws.cell(row=row, column=8, value='= E{current_row}*G{current_row}'.format(current_row=cell.row))
+        cell.number_format = '#,##0.00'
+
+#        cell = ws.cell(row=row, column=6, value='= D{current_row} * E{current_row}'.format(current_row=cell.row))
+#        cell.number_format = openpyxl.styles.numbers.builtin_format_code(3)
+
+    total_row = ws.max_row + 1
+    if total_row > start_row:
+        cell1 = ws.cell(row=total_row, column=7, value='Total')
+        cell1.font = openpyxl.styles.Font(b=True, color='000000', size=15)
+        
+        cell2 = ws.cell(row=total_row, column=8, value='= SUM(H{}:H{})'.format(start_row, total_row - 1))
+        cell2.number_format = openpyxl.styles.numbers.builtin_format_code(3)
+        cell2.border = Border(top=double, left=double, right=double, bottom=double)
+        cell2.font = openpyxl.styles.Font(b=True, color='ff03ce', size=15)
+    
+
+    wb.save(filepath)
+    wb.close()
+
+
+##if __name__ == '__main__':
+#write_to_file('C:\\Users\\pjmk0\\AppData\\Roaming\\test.xlsx')
+if exportBln:
+    save_name = f'{SheetName} CalcSheet({PjtName}_{BldgName})_' + Today.strftime("%Y-%m-%d")+'.xlsx'
+    path = os.getenv('APPDATA').replace('\\','\\\\')
+    filePath = path + '\\' + save_name
+    
+    write_to_file(filePath)
+else:
+    pass
+
+# Assign your output to the OUT variable.
+OUT = os.getenv('APPDATA')
