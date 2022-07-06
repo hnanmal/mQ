f829436ec4ee25456664022c09a168d765810ae5
pjmk007 <pjmk007@gmail.com>
2022-07-06 17:29:00 +0900

    zcZxc

--- _study/CljStudy/my-pjt/src/my_pjt/core.clj
+++ _study/CljStudy/my-pjt/src/my_pjt/core.clj
@@ -1,14 +1,19 @@
 (ns my-pjt.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io]))
 
 (import java.time.LocalDate)
+(use 'dk.ative.docjure.spreadsheet)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   ;; (println (. LocalDate now)))
-  (println
-   (str (LocalDate/now))))
+  ;; (println
+  ;;   (str (LocalDate/now)))
 
-
-(-main)
+  (print 
+    (->> (load-workbook-from-file "11.xlsx")
+       (select-sheet "Sheet1")
+       (select-columns {:A :name, :B :price})))
+  )
