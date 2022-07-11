7e543c82cd9ff50c4436245f0a96d0574155c540
hnanmal <60164230+hnanmal@users.noreply.github.com>
2022-07-12 08:32:37 +0900

    erer

--- _study/CljStudy/tutorial/src/tutorial/core.clj
+++ _study/CljStudy/tutorial/src/tutorial/core.clj
@@ -2,41 +2,36 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
-(defn can-vote
-  [age]
-  (if (>= age 18)
-    (println "You can vote")
-    (println "You can't vote")))
+(use 'clojure.java.io)
 
-(defn can-do-more
-  [age]
-  (if (>= age 18)
-    (do (println "You can drive")
-        (println "You can Vote"))
-    (println "You can't vote")))
+(defn write-to-file
+  [file text]
+  (with-open [wrtr (writer file)]
+    (.write wrtr text)))
 
-(defn when-ex
-  [tof]
-  (when tof
-    (println "1st thing")
-    (println "2nd thing")))
+(defn read-from-file
+  [file]
+  (try
+    (println (slurp file))
+  
+  (catch Exception e (println "Error : " (.getMessage e)))))
 
-(defn what-grade
-  [n]
-  (cond
-    (< n 5) (println "Preschool")
-    (= n 5) (println "Kindergarten")
-    (and (> n 5) (<= n 18)) (format "Go to grade %d" (- n 5))
-    :else "Go to College"))
+(defn append-to-file
+  [file text]
+  (with-open [wrtr (writer file :append true)]
+    (.write wrtr text)))
+
+(defn read-line-from-file
+  [file]
+  (with-open [rdr (reader file)]
+    (doseq [line (line-seq rdr)]
+      (println line))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-
-  (can-vote 17)
-  (can-do-more 24)
-  (when-ex true)
-  (what-grade 19)
-  (what-grade 17)
-  
+  (write-to-file "test.txt" "This is a sentence\n")
+  (read-from-file "test.txt")
+  (append-to-file "test.txt" "This is another sentence\n")
+  (read-line-from-file "test.txt")
   ) 
