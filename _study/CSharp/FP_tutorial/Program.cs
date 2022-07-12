4006fb2b07c009d5cb3ddb7ae348918aea8f3d7c
pjmk007 <pjmk007@gmail.com>
2022-07-12 16:54:09 +0900

    init

---
+++ _study/CSharp/FP_tutorial/Program.cs
@@ -0,0 +1,29 @@
+﻿//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Func<int, int> f = (x) => x + 2;
+//        int i = f(1);
+//        Console.WriteLine(i);
+
+//        f = (x) => 2 * x + 1;
+//        i = f(1);
+//        Console.WriteLine(i);
+//    }
+//}
+
+class Program
+{
+    delegate int DoubleAction(int inp);
+
+    static void Main(string[] args)
+    {
+        DoubleAction da = Double;
+        int doubledValue = da(2);
+    }
+
+    static int Double(int input)
+    {
+        return input * 2;
+    }
+}
