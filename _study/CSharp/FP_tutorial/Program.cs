66c4f5f176e644e4a2c196b4922647e6760dc860
hnanmal <60164230+hnanmal@users.noreply.github.com>
2022-07-13 12:19:22 +0900

    asdf

--- _study/CSharp/FP_tutorial/Program.cs
+++ _study/CSharp/FP_tutorial/Program.cs
@@ -12,18 +12,91 @@
 //    }
 //}
 
-class Program
+//class Program
+//{
+//    delegate int DoubleAction(int inp);
+
+//    static void Main(string[] args)
+//    {
+//        DoubleAction da = Double;
+//        int doubledValue = da(2);
+//    }
+
+//    static int Double(int input)
+//    {
+//        return input * 2;
+//    }
+//}
+
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Func<int, int> da =
+//            (i) => i * 2;
+
+//        int doubledValue = da(2);
+
+//        int i = da(1);
+//    }
+//}
+
+
+//class Program
+//{
+//    //private static string strValue = "First";
+
+//    //public static void AddSpace(string str)
+//    //{
+//    //    strValue += ' ' + str;
+//    //}
+
+//    //static void Main(string[] args)
+//    //{
+//    //    AddSpace("Second!");
+//    //    AddSpace("Third!!");
+//    //    Console.WriteLine(strValue);
+//    //}
+
+//    public static string AddSpace(string strSource, string str)
+//    {
+//        return (strSource + " " + str);
+//    }
+
+//    static void Main(string[] args)
+//    {
+//        string str1 = "First";
+//        string str2 = AddSpace(str1, "Second");
+//        string str3 = AddSpace(str2, "Third");
+//        Console.WriteLine(str3);
+//    }
+//}
+
+
+
+
+public class Program
 {
-    delegate int DoubleAction(int inp);
-
-    static void Main(string[] args)
+    public static int GetFactorial(int intNumber)
     {
-        DoubleAction da = Double;
-        int doubledValue = da(2);
+        if (intNumber == 0)
+        {
+            return 1;
+        }
+
+        return intNumber * GetFactorial(intNumber - 1);
     }
 
-    static int Double(int input)
-    {
-        return input * 2;
-    }
+    //static void Main(string[] args)
+    //{
+    //    Console.WriteLine(
+    //        "Enter an integer number (Imperative approach)");
+    //    int inputNumber = Convert.ToInt32(Console.ReadLine());
+    //    int factorialNumber = GetFactorial(inputNumber);
+    //    Console.WriteLine(
+    //        "{0}! is {1}",
+    //        inputNumber,
+    //        factorialNumber);
+    //}
 }
