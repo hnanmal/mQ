c9eb6cb15516c608de6640727f5e0e8efe847783
hnanmal <60164230+hnanmal@users.noreply.github.com>
2022-07-20 22:20:36 +0900

    dfd

--- _study/CljStudy/my-pjt/src/my_pjt/core.clj
+++ _study/CljStudy/my-pjt/src/my_pjt/core.clj
@@ -1,33 +1,68 @@
 (ns my-pjt.core
   (:gen-class)
-  (:require [clojure.java.io :as io]))
+  ;; (:require [dk.ative.docjure.spreadsheet])
+  )
 
+(use `dk.ative.docjure.spreadsheet)
 (import java.time.LocalDate)
-(use 'dk.ative.docjure.spreadsheet)
+(import java.string)
+
+(defn value [x]
+(case x
+5 "x is 5"
+10 "x is 10"
+"x isn't 5 or 10"))
+(value 11) ;;"x isn't 5 or 10"
+(value 10)
+(value 4)
+
+(defprotocol StrConvert
+  (to-str [this]))
+
+(extend-protocol StrConvert
+  nil
+  (to-str [this] "null")
+  java.lang.Integer
+  (to-str [this] (str this))
+  java.lang.String
+  (to-str [this] (str "\"" this "\""))
+  clojure.lang.Keyword
+  (to-str [this] (to-str (name this)))
+  java.lang.Object
+  (to-str [this] (str this)))
+
+(defn remove-line-break
+  [x] 
+  (cond
+    (nil? x)
+      "null"
+    (= (type x) java.lang.String)
+      (.replaceAll x "\n" " ")))
+  ;; (case x
+  ;;   java.lang.String (.replaceAll x "\n" " ")
+  ;;   x))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  ;; (println (. LocalDate now)))
-  ;; (println
-  ;;   (str (LocalDate/now)))
 
+  ;;  (map #(.getNumericCellValue %)) 
 
-  ;;  (def sheet-mine 
-  ;;    (->> (load-workbook-from-file "11.xlsx")
-  ;;         (select-sheet "Sheet1"))) 
+  (def each-row (->> (load-workbook "building_list.xlsx")
+                     (select-sheet "Building List(Rev.B)")
+                     row-seq
+                     (remove nil?)
+                     (map cell-seq)
+                     (map #(map read-cell %))
+                     (map #(map remove-line-break %))))
 
-  ;; (->> (load-workbook "11.xlsx")
-  ;;      (select-sheet "Sheet1")
-  ;;      row-seq)
+  (def Item-3rd 
+    (nth each-row 3))
 
-(println
- (->> (load-workbook "11.xlsx")
-      (select-sheet "Sheet1")
-      row-seq
-      (remove nil?)
-      (map cell-seq)
-      (map #(map read-cell %))))
-
-      ;;  (select-columns {:B :name, :C :price})
+  ;; ;; (println (.getNumericCellValue firstItem))
+  (println (nth Item-3rd 10))
+  
+  
+  (= (type (nth Item-3rd 10)) java.lang.String)
   )
