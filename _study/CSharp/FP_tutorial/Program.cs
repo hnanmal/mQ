2ecd51a4b9ad7f3fc6b7bc8b956eb8c4996501d6
hnanmal <60164230+hnanmal@users.noreply.github.com>
2022-07-31 23:07:18 +0900

    sdfsf

--- _study/CSharp/FP_tutorial/Program.cs
+++ _study/CSharp/FP_tutorial/Program.cs
@@ -76,27 +76,114 @@
 
 
 
-public class Program
+//public class Program
+//{
+//    public static int GetFactorial(int intNumber)
+//    {
+//        if (intNumber == 0)
+//        {
+//            return 1;
+//        }
+
+//        return intNumber * GetFactorial(intNumber - 1);
+//    }
+
+//    //static void Main(string[] args)
+//    //{
+//    //    Console.WriteLine(
+//    //        "Enter an integer number (Imperative approach)");
+//    //    int inputNumber = Convert.ToInt32(Console.ReadLine());
+//    //    int factorialNumber = GetFactorial(inputNumber);
+//    //    Console.WriteLine(
+//    //        "{0}! is {1}",
+//    //        inputNumber,
+//    //        factorialNumber);
+//    //}
+//}
+
+//public partial class Program
+//{
+//    public static int f(int x)
+//    {
+//        return (4 * x * x - 14 * x - 8);
+//    }
+//}
+
+//public partial class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int i = f(5);
+//        Console.WriteLine(i);
+//    }
+//}
+
+//public partial class Program
+//{
+//    //public static string GetSign(int val)
+//    //{
+//    //    string pos0rNeg;
+
+//    //    if (val > 0)
+//    //        pos0rNeg = "positive";
+//    //    else
+//    //        pos0rNeg = "negative";
+
+//    //    return pos0rNeg;
+//    //}
+//    public static string GetSign(int val)
+//    {
+//        return val > 0 ? "positive" : "negative";
+//    }
+//}
+
+//public partial class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine(
+//            "Sign of -15 is {0}",
+//            GetSign(-15));
+//    }
+//}
+
+public partial class Program
 {
-    public static int GetFactorial(int intNumber)
+    static List<int> NthFunctional(List<int> list, int n)
     {
-        if (intNumber == 0)
+        return list.Where((x, i) => i % n == 0).ToList();
+    }
+}
+
+public partial class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> listing =
+            new List<int>()
+            {
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+            };
+        var list3rd_funct = NthFunctional(listing, 3);
+        PrintIntList("Nth Functional", list3rd_funct);
+    }
+}
+
+public partial class Program
+{
+    static void PrintIntList(
+        string titleHeader,
+        List<int> list)
+    {
+        Console.WriteLine(
+            String.Format("{0}",
+            titleHeader));
+
+        foreach (int i in list)
         {
-            return 1;
+            Console.Write(String.Format("{0}\t", i));
         }
 
-        return intNumber * GetFactorial(intNumber - 1);
+        Console.WriteLine("\n");
     }
-
-    //static void Main(string[] args)
-    //{
-    //    Console.WriteLine(
-    //        "Enter an integer number (Imperative approach)");
-    //    int inputNumber = Convert.ToInt32(Console.ReadLine());
-    //    int factorialNumber = GetFactorial(inputNumber);
-    //    Console.WriteLine(
-    //        "{0}! is {1}",
-    //        inputNumber,
-    //        factorialNumber);
-    //}
 }
