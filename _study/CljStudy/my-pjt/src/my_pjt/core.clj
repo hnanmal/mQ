d68fb41e3644b2711b82e41b29dd81ad7f1ac4ae
pjmk007 <pjmk007@gmail.com>
2022-08-03 17:01:22 +0900

    dffdf

--- _study/CljStudy/my-pjt/src/my_pjt/core.clj
+++ _study/CljStudy/my-pjt/src/my_pjt/core.clj
@@ -1,68 +1,28 @@
 (ns my-pjt.core
   (:gen-class)
-  ;; (:require [dk.ative.docjure.spreadsheet])
-  )
+  (:require [clojure.java.io :as io]))
 
-(use `dk.ative.docjure.spreadsheet)
 (import java.time.LocalDate)
-(import java.string)
-
-(defn value [x]
-(case x
-5 "x is 5"
-10 "x is 10"
-"x isn't 5 or 10"))
-(value 11) ;;"x isn't 5 or 10"
-(value 10)
-(value 4)
-
-(defprotocol StrConvert
-  (to-str [this]))
-
-(extend-protocol StrConvert
-  nil
-  (to-str [this] "null")
-  java.lang.Integer
-  (to-str [this] (str this))
-  java.lang.String
-  (to-str [this] (str "\"" this "\""))
-  clojure.lang.Keyword
-  (to-str [this] (to-str (name this)))
-  java.lang.Object
-  (to-str [this] (str this)))
-
-(defn remove-line-break
-  [x] 
-  (cond
-    (nil? x)
-      "null"
-    (= (type x) java.lang.String)
-      (.replaceAll x "\n" " ")))
-  ;; (case x
-  ;;   java.lang.String (.replaceAll x "\n" " ")
-  ;;   x))
-
+(use 'dk.ative.docjure.spreadsheet)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  ;; (println (. LocalDate now)))
+  ;; (println
+  ;;   (str (LocalDate/now)))
 
-  ;;  (map #(.getNumericCellValue %)) 
 
-  (def each-row (->> (load-workbook "building_list.xlsx")
-                     (select-sheet "Building List(Rev.B)")
-                     row-seq
-                     (remove nil?)
-                     (map cell-seq)
-                     (map #(map read-cell %))
-                     (map #(map remove-line-break %))))
+  (println
+  (->> (load-workbook-from-file ".\\11.xlsx") ;; (load-workbook "building_list.xlsx")
+        (select-sheet "Sheet1")
+        (select-columns {:B :first :C :third})
+        (remove nil?)
+        ;; row-seq
+        ;; (remove nil?)
+        ;; (map cell-seq)
+        ;; (map #(map read-cell %))
+        ))
 
-  (def Item-3rd 
-    (nth each-row 3))
-
-  ;; ;; (println (.getNumericCellValue firstItem))
-  (println (nth Item-3rd 10))
-  
-  
-  (= (type (nth Item-3rd 10)) java.lang.String)
+      ;;  (select-columns {:B :name, :C :price})
   )
