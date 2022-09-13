ceede361b774456f915f26a069b5430b47c7b1d8
hnanmal <60164230+hnanmal@users.noreply.github.com>
2022-09-14 00:43:18 +0900

    asdff

--- _study/CSharp/FP_tutorial/Program.cs
+++ _study/CSharp/FP_tutorial/Program.cs
@@ -1,39 +1,44 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-////public partial class Program
-////{
-////    public static string GetSign(int val)
-////    {
-////        string posOrNeg;
+public partial class Program
+{
+    public static int NonCurriedAdd(int a, int b) => a + b;
+}
 
-////        if (val > 0)
-////            posOrNeg = "positive";
-////        else
-////            posOrNeg = "negative";
+public partial class Program
+{
+    static void Main(string[] args)
+    {
+        int add = NonCurriedAdd(2, 3);
+        Console.WriteLine(add);
+    }
+}
 
-////        return posOrNeg;
-////    }
-////}
+public partial class Program
+{
+    public static Func<int, int> CurriedAdd(int a) => b => a + b;
+}
 
+public partial class Program
+{
+    public static void CurriedStyle()
+    {
+        int add = CurriedAdd(2)(3);
+        Console.WriteLine(add);
+    }
+}
 
-//public partial class Program
-//{
-//    static void Main(string[] args)
-//    {
-//        Console.WriteLine(
-//            "Sign of -15 is {0}",
-//            GetSign(-15));
-//    }
-//}
+public partial class Program
+{
+    public static void CurriedStyle2()
+    {
+        var addition = CurriedAdd(2);
 
-//public partial class Program
-//{
-//    public static string GetSign(int val)
-//    {
-//        return val > 0 ? "positive" : "negative";
-//    }
-//}
+        int x = addition(3);
+        Console.WriteLine(x);
+    }
+}
