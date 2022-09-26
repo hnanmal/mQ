f36de57e3c670dc849ae036491d9336b2d645b1c
pjmk007 <pjmk007@gmail.com>
2022-09-26 17:35:54 +0900

    asdasdf

---
+++ _study/CSharp/FP_ch2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+public partial class Program
+{
+    public delegate void SingleStringDelegate(string dataString);
+
+    private static void AssignData(string dataString)
+    {
+        var globalString = dataString;
+    }
+
+    private static void WriteToConsole(string dataText)
+    {
+        Console.WriteLine(dataText);
+    }
+}
+
