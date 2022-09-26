4a1b2df884943c864cc120daccd9c4bedb8304a8
hnanmal <60164230+hnanmal@users.noreply.github.com>
2022-09-27 00:27:59 +0900

    asdf

--- _study/CSharp/FP_ch2/Program.cs
+++ _study/CSharp/FP_ch2/Program.cs
@@ -1,17 +1,39 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 public partial class Program
 {
-    public delegate void SingleStringDelegate(string dataString);
-
-    private static void AssignData(string dataString)
+    static int Rectangle(int a, int b)
     {
-        var globalString = dataString;
-    }
-
-    private static void WriteToConsole(string dataText)
-    {
-        Console.WriteLine(dataText);
+        return a * b;
     }
 }
 
+public partial class Program
+{
+    private delegate int AreaCalculatorDelegate(int x, int y);
+}
+
+public partial class Program
+{
+    static int Square(int x, int y)
+    {
+        return x * y;
+    }
+}
+
+public partial class Program
+{
+    static void Main(string[] args)
+    {
+        AreaCalculatorDelegate rect = Rectangle;
+        AreaCalculatorDelegate sqr = Square;
+        int i = rect(1, 2);
+        int j = sqr(2, 3);
+        Console.WriteLine("i = " + i);
+        Console.WriteLine("j = " + j);
+    }
+}
---
+++ _study/CSharp/FP_ch2/Program1.cs
@@ -0,0 +1,21 @@
+﻿
+
+//public partial class Program
+//{
+//    public delegate void SingleStringDelegate(string dataString);
+
+//    private static void AssignData(string dataString)
+//    {
+//        var globalString = dataString;
+//    }
+
+//    private static void WriteToConsole(string dataText)
+//    {
+//        Console.WriteLine(dataText);
+//    }
+
+//    SingleStringDelegate delegate1 = AssignData;
+
+//    SingleStringDelegate delegate2 = WriteToConsole;
+//}
+
