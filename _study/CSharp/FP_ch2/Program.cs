2db57974fc75c1e688e878872dda8a5f507608d6
pjmk007 <pjmk007@gmail.com>
2022-09-29 17:35:06 +0900

    dfdfdf

--- _study/CSharp/FP_ch2/Program.cs
+++ _study/CSharp/FP_ch2/Program.cs
@@ -72,7 +72,72 @@ namespace CombineDelegates
         }
     static void Main(string[] args)
         {
-            CombineDelegate();
+            DuplicateEntries();
+        }
+    }
+
+    public partial class Program
+    {
+        private static void RemoveDelegate()
+        {
+            CalculatorDelegate addDel = Add;
+            CalculatorDelegate subDel = Subtract;
+            CalculatorDelegate mulDel = Multiply;
+            CalculatorDelegate divDel = Division;
+            CalculatorDelegate calcDelegates1 =
+                (CalculatorDelegate)Delegate.Combine(
+                    addDel,
+                    subDel);
+            CalculatorDelegate calcDelegates2 =
+                (CalculatorDelegate)Delegate.Combine(
+                    calcDelegates1,
+                    mulDel);
+            CalculatorDelegate calcDelegates3 =
+                (CalculatorDelegate)Delegate.Combine(
+                    calcDelegates2,
+                    divDel);
+            Console.WriteLine(
+                "Total delegates in calcDelegates3: {0}",
+                calcDelegates3.GetInvocationList().Length);
+            calcDelegates3(6, 3);
+            CalculatorDelegate calcDelegates4 =
+                (CalculatorDelegate)Delegate.Remove(
+                    calcDelegates3,
+                    mulDel);
+            Console.WriteLine(
+                "Total delegates in calcDelegates4: {0}",
+                calcDelegates4.GetInvocationList().Length);
+            calcDelegates4(6, 3);
+        }
+    }
+
+    public partial class Program
+    {
+        private static void DuplicateEntries()
+        {
+            CalculatorDelegate addDel = Add;
+            CalculatorDelegate subDel = Subtract;
+            CalculatorDelegate MulDel = Multiply;
+            CalculatorDelegate duplicateDelegates1 =
+                (CalculatorDelegate)Delegate.Combine(
+                    addDel,
+                    subDel);
+            CalculatorDelegate duplicateDelegates2 =
+                (CalculatorDelegate)Delegate.Combine(
+                    duplicateDelegates1,
+                    MulDel);
+            CalculatorDelegate duplicateDelegates3 =
+                (CalculatorDelegate)Delegate.Combine(
+                    duplicateDelegates2,
+                    subDel);
+            CalculatorDelegate duplicateDelegates4 =
+                (CalculatorDelegate)Delegate.Combine(
+                    duplicateDelegates3,
+                    addDel);
+            Console.WriteLine(
+                "Total delegates in duplicateDelegates4: {0}",
+                duplicateDelegates4.GetInvocationList().Length);
+            duplicateDelegates4(6, 3);
         }
     }
 }
