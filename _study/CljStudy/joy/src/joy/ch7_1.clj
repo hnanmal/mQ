bc629eb2ef8eca7cdd2c5b8ccaf975f91de6d59f
hnanmal <60164230+hnanmal@users.noreply.github.com>
2022-10-11 01:55:05 +0900

    afasf

--- _study/CljStudy/joy/src/joy/ch7_1.clj
+++ _study/CljStudy/joy/src/joy/ch7_1.clj
@@ -22,3 +22,20 @@
                (take (dec n) (repeat rest)))))
 
 ((fnth 5) '[a b c d e])
+
+(cons first
+      (take (dec 4) (repeat rest)))
+
+(apply comp
+       (cons first
+             (take (dec 4) (repeat rest))))
+
+(map (comp  ; 함수 구성
+      keyword  ; 키워드 생성
+      #(.toLowerCase %)  ; 소문자로 변환
+      name)  ; 이름을 string으로 리턴
+     '(a B C))  ; 합성한 함수를 심벌의 리스트에 맵핑
+
+;;;
+((partial + 5) 100 200)
+
