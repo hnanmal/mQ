74918cac8098c1b189e11b174e2c34871101ef44
pjmk007 <pjmk007@gmail.com>
2022-10-13 17:31:31 +0900

    adsaf

---
+++ _study/CSharp/FP_ch4/ExtendingCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtendingInterface;
+
+namespace ExtendingInterface
+{
+    public static class IDataSourceExtension
+    {
+        public static IEnumerable<DataItem> GetItemsByGender(
+            this IDataSource src,
+            string gender)
+        {
+            foreach (DataItem item in src.GetItems())
+            {
+                if (item.Gender == gender)
+                    yield return item;
+            }
+        }
+    }
+}
+namespace FP_ch4
+{
+    public static partial class IDataSourceCollectionExtension
+    {
+        public static IEnumerable<DataItem> GetAllItemsByGender_IEnum(
+            this IEnumerable src, string gender)
+        {
+            var items = new List<DataItem>();
+            foreach (var s in src)
+            {
+                var refDataSource = s as IDataSource;
+                if (refDataSource != null)
+                {
+                    items.AddRange(refDataSource.GetItemsByGender(gender));
+                }
+            }
+            return items;
+        }
+    }
+}
---
+++ _study/CSharp/FP_ch4/Interface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendingInterface
+{
+    public interface IDataSource
+    {
+        IEnumerable<DataItem> GetItems();
+    }
+}
