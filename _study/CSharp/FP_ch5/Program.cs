083dbc20542ece22b6a1eca46cc3a8ff91b04a81
pjmk007 <pjmk007@gmail.com>
2022-11-03 17:28:05 +0900

    ASDAsd

--- _study/CSharp/FP_ch5/Program.cs
+++ _study/CSharp/FP_ch5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Diagnostics.Metrics;
 using System.Linq;
 
 namespace SequencesAndElements
@@ -8,7 +9,7 @@ namespace SequencesAndElements
     {
         static void Main(string[] args)
         {
-            ExtractArray();
+            DeferredExecution();
         }
     }
     public partial class Program
@@ -62,5 +63,66 @@ namespace SequencesAndElements
             }
             return i != 1;
         }
+    }
+
+    public partial class Program
+    {
+        public static void DeferredExecution()
+        {
+            List<Member> memberList = new List<Member>()
+            {
+                new Member
+                {
+                    ID = 1,
+                    Name = "Eddie Morgan",
+                    Gender = "Male",
+                    MemberSince = new DateTime(2016, 2, 10)
+                },
+                new Member
+                {
+                    ID = 2,
+                    Name = "Millie Duncan",
+                    Gender = "Female",
+                    MemberSince = new DateTime(2015, 4, 3)
+                },
+                new Member
+                {
+                    ID = 3,
+                    Name = "Thiago Hubbard",
+                    Gender = "Male",
+                    MemberSince = new DateTime(2014, 1, 8)
+                },
+                new Member
+                {
+                    ID = 4,
+                    Name = "Emilia Shaw",
+                    Gender = "Female",
+                    MemberSince = new DateTime(2015, 11, 15)
+                }
+            };
+            IEnumerable<Member> memberQuery =
+                from m in memberList
+                where m.MemberSince.Year > 2014
+                orderby m.Name
+                select m;
+            memberList.Add(new Member
+            {
+                ID = 5,
+                Name = "Chloe Day",
+                Gender = "Female",
+                MemberSince = new DateTime(2016, 5, 28)
+            });
+            foreach (Member m in memberQuery)
+            {
+                Console.WriteLine(m.Name);
+            }
+        }
+    }
+    public class Member
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime MemberSince { get; set; }
     }
 }
