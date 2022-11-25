d0bbc066b63d3645fd4148c4415f85231c02cdc1
pjmk007 <pjmk007@gmail.com>
2022-11-25 17:28:53 +0900

    aasdfsa

--- _study/CSharp/FP_ch5/Program.cs
+++ _study/CSharp/FP_ch5/Program.cs
@@ -9,7 +9,7 @@ namespace SequencesAndElements
     {
         static void Main(string[] args)
         {
-            NonDeferred();
+            UsingStaticMethod();
         }
     }
 
@@ -154,5 +154,59 @@ namespace SequencesAndElements
         public string Name { get; set; }
         public string Gender { get; set; }
         public DateTime MemberSince { get; set; }
+    }
+
+    public partial class Program
+    {
+        private static void UsingExtensionMethod()
+        {
+            IEnumerable<string> query =
+            names
+                .Where(n => n.Length > 4)
+                .OrderBy(n => n[0])
+                .Select(n => n.ToUpper());
+
+            //IEnumerable<string> query = Enumerable.Select(
+            //    Enumerable.OrderBy(Enumerable.Where(names, n => n.Length > 4),
+            //    n => n[0]), n => n.ToUpper());
+
+            foreach (string s in query)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+
+    public partial class Program
+    {
+        private static void UsingStaticMethod()
+        {
+            IEnumerable<string> query = 
+                Enumerable.Select(
+                    Enumerable.OrderBy(
+                        Enumerable.Where(
+                            names, n => n.Length > 4),
+                    n => n[0]), n => n.ToUpper());
+            foreach (string s in query)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+    public partial class Program
+    {
+        static List<string> names = new List<string>
+        {
+            "Howard", "Pat",
+            "Jaclyn", "Kathryn",
+            "Ben", "Aaron",
+            "Stacey", "Levi",
+            "Patrick", "Tara",
+            "Joe", "Ruby",
+            "Bruce", "Cathy",
+            "Jimmy", "Kim",
+            "Kelsey", "Becky",
+            "Scott", "Dick"
+        };
     }
 }
