26d35f22580eb8948a98d7649db44d45497429bb
pjmk007 <pjmk007@gmail.com>
2022-12-01 17:41:49 +0900

    asdfasf

--- _study/CSharp/FP_ch5/Program.cs
+++ _study/CSharp/FP_ch5/Program.cs
@@ -10,7 +10,7 @@ namespace SequencesAndElements
     {
         static void Main(string[] args)
         {
-            SimplyTakeAndSkipOperator();
+            TakeWhileAndSkipWhileOperators();
         }
     }
 
@@ -249,6 +249,75 @@ namespace SequencesAndElements
             Console.WriteLine();
         }
     }
+
+    public partial class Program
+    {
+        public static void NoTakeSkipOperator()
+        {
+            IEnumerable<int> intCollection =
+                Enumerable.Range(1, 1000000);
+            IEnumerable<int> hugeQuery =
+                intCollection
+                    .Where(h => h % 2 == 0 && h % 7 == 0);
+            foreach (int x in hugeQuery)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+    public partial class Program
+    {
+        public static void TakeAndSkipOperator()
+        {
+            IEnumerable<int> intCollection =
+                Enumerable.Range(1, 1000000);
+            IEnumerable<int> hugeQuery =
+                intCollection
+                    .Where(h => h % 2 == 0 && h % 7 == 0);
+            int pageSize = 10;
+            for (int i = 0; i < hugeQuery.Count() / pageSize; i++)
+            {
+                IEnumerable<int> paginationQuery =
+                    hugeQuery
+                        .Skip(i * pageSize)
+                        .Take(pageSize);
+                foreach (int x in paginationQuery)
+                {
+                    Console.WriteLine(x);
+                }
+                Console.WriteLine(
+                    "Press Enter to continue, " +
+                    "other key will stop process!");
+                if (Console.ReadKey().Key != ConsoleKey.Enter)
+                    break;
+            }
+        }
+    }
+
+    public partial class Program
+    {
+        public static void TakeWhileAndSkipWhileOperators()
+        {
+            int[] intArray = { 10, 4, 27, 53, 2, 96, 48 };
+            IEnumerable<int> queryTakeWhile =
+                intArray.TakeWhile(n => n < 50);
+            Console.WriteLine("TakeWhile operator");
+            foreach (int i in queryTakeWhile)
+            {
+                Console.WriteLine(String.Format("{0}\t", i));
+            }
+            Console.WriteLine();
+            IEnumerable<int> querySkipWhile =
+                intArray.SkipWhile(n => n < 50);
+            Console.WriteLine("SkipWhile operator");
+            foreach (int i in querySkipWhile)
+            {
+                Console.WriteLine(String.Format("{0}\t", i));
+            }
+            Console.WriteLine();
+        }
+    }
+
     public partial class Program
     {
         static List<int> intList = new List<int>
