45a42cb0232973cf1a8d4bf805fd9e36a1afbace
pjmk007 <pjmk007@gmail.com>
2022-12-15 23:21:30 +0900

    asdf

---
+++ _study/CljStudy/joy/src/joy/ch9_2.clj
@@ -0,0 +1,91 @@
+(ns joy.udp
+  (:refer-clojure :exclude [get]))  ; clojure.core/get을 참조에서 제외
+
+
+(defn beget [this proto]
+  (assoc this ::prototype proto))  ; 맵을 받아서 :joy.udp/prototype 키에 프로토타입을 연결한다.
+
+
+(beget {:sub 0} {:super 1})
+
+
+(defn get [m k]
+  (when m
+    (if-let [[_ v] (find m k)]
+      v
+      (recur (::prototype m) k))))
+
+(get (beget {:sub 0} {:super 1})
+     :super)
+
+
+(def put assoc)
+
+
+;;;;
+
+(def cat {:likes-dogs true, :ocd-bathing true})
+(def morris (beget {:likes-9lives true} cat))
+(def post-traumatic-morris (beget {:likes-dogs nil} morris))
+
+(get cat :likes-dogs)
+
+
+(get morris :likes-dogs)
+
+post-traumatic-morris
+
+(get post-traumatic-morris :likes-dogs)
+
+
+(get post-traumatic-morris :likes-9lives)
+
+
+;;;;;
+
+(defmulti compiler :os)
+(defmethod compiler ::unix [m] (get m :c-compiler))
+(defmethod compiler ::osx [m] (get m :llvm-compiler))
+
+(def clone (partial beget {}))
+(def unix {:os ::unix, :c-compiler "cc", :home "/home", :dev "/dev"})
+(def osx (-> (clone unix)
+             (put :os ::osx)
+             (put :llvm-compiler "clang")
+             (put :home "/Users")))
+
+(compiler unix)
+
+
+(compiler osx)
+
+unix
+osx
+
+(defmulti home :os)
+(defmethod home ::unix [m] (get m :home))
+
+(home unix)
+
+
+(home osx)
+
+(derive ::osx ::unix)
+
+
+(home osx)
+;;
+
+(parents ::osx)
+
+
+(ancestors ::osx)
+
+
+(descendants ::unix)
+
+
+(isa? ::osx ::unix)
+
+
+(isa? ::unix ::osx)
