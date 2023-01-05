0f2d9d7134e0a40bb54f5361780d075e3a642e61
pjmk007 <pjmk007@gmail.com>
2023-01-05 12:16:50 +0900

    Update App.java

--- _study/java/vscode/Hello/src/App.java
+++ _study/java/vscode/Hello/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        java.util.HashMap props = new java.util.HashMap<>();
+        props.put("HOME", "home/me");    /* 일부 코드 생략 */
+        props.put("SRC", "src");
+        props.put("BIN", "classes");
+        System.out.println(props);
     }
 }
