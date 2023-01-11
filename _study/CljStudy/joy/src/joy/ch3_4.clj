5b509a261cb74b2e49a4e3acf1b2fba5277c6145
hnanmal <60164230+hnanmal@users.noreply.github.com>
2023-01-11 20:58:59 +0900

    Update ch3_4.clj

--- _study/CljStudy/joy/src/joy/ch3_4.clj
+++ _study/CljStudy/joy/src/joy/ch3_4.clj
@@ -63,10 +63,28 @@ frame
   (for [x (range max-x) y (range max-y)]
     [x y (rem (bit-xor x y) 256)]))
 
-(defn clear [g] (.clearRect g 0 0 200 200))
+(defn clear [g] (.clearRect g 0 0 256 256))
 
 (clear gfx)
 
 (doseq [[x y xor] (xors 500 500)]
   (.setColor gfx (java.awt.Color. xor xor xor))
   (.fillRect gfx x y 1 1))
+
+
+;;;;;3.4.5 재미삼아 해보기
+
+(defn f-values [f xs ys]
+  (for [x (range xs) y (range ys)]
+    [x y (rem (f x y) 256)]))
+
+(defn draw-values [f xs ys]
+  (clear gfx)
+  (.setSize frame (java.awt.Dimension. xs ys))
+  (doseq [[x y v] (f-values f xs ys)]
+    (.setColor gfx (java.awt.Color. v v v))
+    (.fillRect gfx x y 50 50)))
+
+(draw-values bit-and 256 256)
+(draw-values + 256 256)
+(draw-values * 256 256)
