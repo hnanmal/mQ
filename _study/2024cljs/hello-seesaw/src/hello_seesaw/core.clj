fa861cf0e793ea552c4a3d8e1bee9babbc2452cb
hnanmal <60164230+hnanmal@users.noreply.github.com>
2024-01-16 00:10:56 +0900

    Update core.clj

--- _study/2024cljs/hello-seesaw/src/hello_seesaw/core.clj
+++ _study/2024cljs/hello-seesaw/src/hello_seesaw/core.clj
@@ -1,6 +1,20 @@
 (ns hello-seesaw.core
   (:use seesaw.core))
 
+(require '[seesaw.bind :as b])
+
+(def safety
+  (checkbox :text "Safety"
+             :selected? true))
+
+(def fire-missiles 
+  (button :text "Fire!"
+          :enabled? false))
+
+(b/bind
+ safety
+ (b/property fire-missiles :enabled?))
+
 (defn foo
   "I don't do a whole lot."
   [x]
@@ -9,7 +23,8 @@
 (defn -main [& args]
   (invoke-later
    (-> (frame :title "Hello",
-              :content "Hello, Seesaw",
+              ;; :content "Hello, Seesaw",
+              :content safety,
               :on-close :exit)
        pack!
        show!)))
