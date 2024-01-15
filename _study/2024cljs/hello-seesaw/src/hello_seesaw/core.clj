20767dfec0e580085914663947bb5841e77f3a2b
hnanmal <60164230+hnanmal@users.noreply.github.com>
2024-01-16 00:34:24 +0900

    Update core.clj

--- _study/2024cljs/hello-seesaw/src/hello_seesaw/core.clj
+++ _study/2024cljs/hello-seesaw/src/hello_seesaw/core.clj
@@ -20,11 +20,33 @@
   [x]
   (println x "Hello, World!"))
 
+(def form
+  (grid-panel :columns 2
+              :items ["First Name" (text :id :first-name)
+                      "Last Name" (text :id :last-name)
+                      "Sex" (combobox :id :sex
+                                      :model ["Female" "Male"])
+                      "Age" (spinner :id :age)]))
+
 (defn -main [& args]
   (invoke-later
-   (-> (frame :title "Hello",
-              ;; :content "Hello, Seesaw",
-              :content safety,
-              :on-close :exit)
-       pack!
-       show!)))
+   (->
+    ;; (frame :title "Hello",
+    ;;           ;; :content "Hello, Seesaw",
+    ;;           :content safety,
+    ;;           :on-close :exit) 
+    (frame :title "Why Swing, why?",
+           :on-close :exit
+          ;;  :content (label :text "Hiya"
+          ;;                  :border [100 "Compound" 10]
+          ;;                  :background "#999"
+          ;;                  :foreground :blue)
+          ;;  :content (slider :min 0
+          ;;                   :max 11
+          ;;                   :value 6
+          ;;                   :background "#999"
+          ;;                   :foreground :blue)
+           :content form
+           )
+    pack!
+    show!)))
